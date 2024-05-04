14708aed6e56978fab68608d5beb8db5d8d68391
yurii <robka4an@gmail.com>
2024-05-04 21:26:45 +0300

    finish hw

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,10 +3,6 @@ import ContactList from "../ContactList/ContactList";
 import SearchBox from "../SearchBox/SearchBox";
 
 export default function App() {
-  // const showContact = contacts.filter((contact) =>
-  //   contact.name.toLowerCase().includes(search.toLowerCase())
-  // );
-
   return (
     <div>
       <h1>Phonebook</h1>
