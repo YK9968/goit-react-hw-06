15ea7988cbafb98db2439eeb3f919e45d844fa9e
yurii <robka4an@gmail.com>
2024-05-04 16:54:21 +0300

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,22 +2,22 @@ import { FaUser } from "react-icons/fa6";
 import { FaPhoneAlt } from "react-icons/fa";
 import css from "./Contact.module.css";
 
-export default function Contact({ contact: { name, number, id }, onDelete }) {
+export default function Contact({ contact }) {
+  console.log(contact.id);
+
   return (
     <>
       <div>
         <p>
           <FaUser className={css.iconContact} />
-          {name}
+          {}
         </p>
         <p>
           <FaPhoneAlt className={css.iconContact} />
-          {number}
+          {}
         </p>
       </div>
-      <button onClick={() => onDelete(id)} className={css.btnDelete}>
-        Delete
-      </button>
+      <button className={css.btnDelete}>Delete</button>
     </>
   );
 }
